fdd768851acf31b4b072a2b4ded01293f103661c
Ciensprog <jorg.leonrd@gmail.com>
2022-02-04 12:04:09 -0600

    Install & configure new dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/*
+|--------------------------------------------------------------------------
+| Configuration
+|--------------------------------------------------------------------------
+|
+| ...
+|
+*/
+
+module.exports = {
+  mode: 'jit',
+  purge: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      apple: '#A3DE83',
+      banana: '#F7F39A',
+      'editor-dot': '#D5EDED',
+      grape: '#343352',
+      pitahaya: '#FFBCD1',
+      'purple-gray': '#767594',
+      strawberry: '#FF5677',
+    },
+  },
+  plugins: [],
+}
