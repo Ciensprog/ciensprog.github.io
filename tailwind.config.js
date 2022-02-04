2ae9d90c618d4230c83676410b86ee6024e4d07b
Ciensprog <jorg.leonrd@gmail.com>
2022-02-04 16:10:39 -0600

    Extend configuration | colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,15 +12,16 @@ module.exports = {
   purge: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      apple: '#A3DE83',
-      banana: '#F7F39A',
-      'editor-dot': '#D5EDED',
-      grape: '#343352',
-      pitahaya: '#FFBCD1',
-      'purple-gray': '#767594',
-      strawberry: '#FF5677',
+    extend: {
+      colors: {
+        apple: '#A3DE83',
+        banana: '#F7F39A',
+        'editor-dot': '#D5EDED',
+        grape: '#343352',
+        pitahaya: '#FFBCD1',
+        'purple-gray': '#767594',
+        strawberry: '#FF5677',
+      },
     },
   },
   plugins: [],
